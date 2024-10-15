8567571e453d133f8ed39c7fc84acaa2ff8f4e95
Trần Ngọc Phát <tranngocphat240111@gmail.com>
2024-10-15 16:04:18 +0700

    done 2 trang Tai khoan, Doanh Thu

--- src/GUI/LeTan_DonDatPhong_ThemDichVu.java
+++ src/GUI/LeTan_DonDatPhong_ThemDichVu.java
@@ -118,7 +118,6 @@ public class LeTan_DonDatPhong_ThemDichVu extends javax.swing.JFrame {
         label_DonGia2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(777, 456));
 
         kGradientPanel1.setkEndColor(new java.awt.Color(115, 115, 115));
         kGradientPanel1.setkGradientFocus(250);

e2a7af23472e82abe76183d5d2ae19deb7bbbd96
oeb <22691861.bao@student.iuh.edu.vn>
2024-10-24 16:00:59 +0700

    Update MongoDBConnection.java

--- src/model/MongoDBConnection.java
+++ src/model/MongoDBConnection.java
@@ -5,13 +5,27 @@ import com.mongodb.client.MongoDatabase;
 
 public class MongoDBConnection {
 
-    private MongoClient mongoClient;
-    private MongoDatabase database;
-    String connectionLocal = "mongodb://localhost:27017/";
-    String connectionCloud = "mongodb+srv://admin:admin@atlascluster.mnaai2a.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
-    String databaseName = "Hotel";
+    private static MongoClient mongoClient;
+    private static MongoDatabase database;
+    private static String connectionLocal = "mongodb://localhost:27017/";
+    private static String connectionCloud = "mongodb+srv://admin:admin@atlascluster.mnaai2a.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
+    private static String databaseName = "Hotel";
 
     public MongoDBConnection() {
+    }
+
+    public static MongoDatabase getDatabase() {
+        return database;
+    }
+
+    public static void closeConnection() {
+        if (mongoClient != null) {
+            mongoClient.close();
+            System.out.println("MongoDB connection closed.");
+        }
+    }
+    
+    public static void connection() {
         try {
             mongoClient = MongoClients.create(connectionLocal);
             database = mongoClient.getDatabase(databaseName);
@@ -19,19 +33,5 @@ public class MongoDBConnection {
         } catch (Exception e) {
             System.err.println("Error connecting to MongoDB: " + e.getMessage());
         }
-    }
-
-    public MongoDatabase getDatabase() {
-        return database;
-    }
-
-    public void closeConnection() {
-        if (mongoClient != null) {
-            mongoClient.close();
-            System.out.println("MongoDB connection closed.");
-        }
-    }
-    public static void main(String[] args) {
-        new MongoDBConnection();
     }
 }
